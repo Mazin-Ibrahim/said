7745047a748dc408a956be117e5a5551ea1a38a7
Mazin-Ibrahim <mazinabrahim200@gmail.com>
2023-02-22 11:52:53 +0300

    add api middlware

---
+++ app/Http/Middleware/ResponseApi.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class ResponseApi
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $response = $next($request);
+
+        if (in_array($response->status(), [200, 201, 404, 401, 422,204])) {
+            $response->header('Content-Type', 'application/json');
+        }
+
+        return $response;
+    }
+}
